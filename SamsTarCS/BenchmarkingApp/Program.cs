577861b6dda13c0e9044de1833769940268a5782
Sam Foster <samfozz@gmail.com>
2018-06-07 09:20:39 +0100

    playing with a background writer impl

--- SamsTarCS/BenchmarkingApp/Program.cs
+++ SamsTarCS/BenchmarkingApp/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.IO.Pipes;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using tar_cs;
 
 namespace BenchmarkingApp
 {
     internal class Program
     {
+        private static AsyncFileWriter writer;
+
         public static void Main(string[] args)
         {
+            writer = new AsyncFileWriter(2, 100000000);
+            writer.Start();
+
             var testRuns = 2;
             
             var skip = false;
@@ -20,13 +30,8 @@ namespace BenchmarkingApp
             
             Stopwatch sw = new Stopwatch();
             
-            
-         
-            
             foreach (var testFile in _testDataDir.GetFiles("*.tgz"))
             {
-             
-
                 sw.Reset();
                 sw.Start();
                 
@@ -37,7 +42,10 @@ namespace BenchmarkingApp
                     dirsCreated = filesCreated = 0;
 
                     var testOutDir = tmpDir.CreateSubdirectory(testFile.Name.Replace(".", "_")+"__"+i);
-                
+
+
+                    Console.WriteLine($"File : {testFile.Name} / Pass : {i}");
+
                     using (var tarStream = new GZipStream(File.OpenRead(testFile.FullName), CompressionMode.Decompress))
                     {
                         
@@ -59,7 +67,18 @@ namespace BenchmarkingApp
                 Console.WriteLine($"To unpack '{testFile.Name}' {testRuns} times took '{sw.Elapsed}' - avg - '{sw.Elapsed.TotalMinutes / testRuns} mins per go.'");
             }
 
+            Console.WriteLine("Waiting for file writes to finish ...");
+            sw.Reset();
+            sw.Start();
+            writer.WaitComplete().Wait();
 
+            Console.WriteLine($"Took '{sw.ElapsedMilliseconds}'ms o finish.");
+
+
+            Console.WriteLine("Files here :\r\n " + tmpDir.FullName);
+            Console.ReadKey(true);
+
+            Console.WriteLine("Press to delete folder...");
 
             Directory.Delete(tmpDir.FullName, true);
 
@@ -76,6 +95,8 @@ namespace BenchmarkingApp
 
         private static int dirsCreated = 0;
         private static int filesCreated = 0;
+
+
 
 
 
@@ -112,11 +133,15 @@ namespace BenchmarkingApp
                         createdDirs.Add(file.DirectoryName);
                     }
 
-                    using (var outputStream = File.Create(fullPath))
+                  // using (var outputStream = File.Create(fullPath))
+                    using(var outputStream = new MemoryStream())
                     {
                         filesCreated++;
                         // Read data from a current file to a Stream.
+                         
                         tarReader.Read(outputStream);
+
+                        writer.Enqueue(fullPath, outputStream.ToArray()).Wait();
                     }
                     
                     break;
@@ -127,6 +152,151 @@ namespace BenchmarkingApp
                     break;
                 //     throw new NotSupportedException("Not supported entry type: " + tarReader.FileInfo.EntryType);
             }
+
+             
         }
+
+       
     }
+
+
+
+    class AsyncFileWriter
+    {
+        private int _workerThreads;
+
+        private long _maxSize;
+
+        private long _currentSize = 0;
+
+        public AsyncFileWriter(int workerThreads, long maxSize)
+        {
+            _workerThreads = workerThreads;
+            _maxSize = maxSize;
+        }
+
+        private Dictionary<string, byte[]> _work  =new Dictionary<string, byte[]>();
+
+        private object _lock = new object();
+
+        private Thread[] _workers;
+
+        public long CurrentSize
+        {
+            get { return _currentSize; }
+        }
+
+        public bool HasWork
+        {
+            get { return _work.Count > 0; }
+        }
+
+        public async Task Enqueue(string fileName, byte[] data)
+        {
+             await WaitForRoom(data.Length, TimeSpan.FromSeconds(0.1));
+
+            lock (_lock)
+            {
+                if (_work.ContainsKey(fileName))
+                {
+                    throw new DuplicateNameException();
+                }
+
+                _work[fileName] = data;
+                _currentSize += data.Length;
+            }
+        }
+
+
+        public void Start()
+        {
+            _workers = new Thread[_workerThreads];
+
+            for (var i = 0; i < _workerThreads; i++)
+            {
+                _workers[i] = new Thread(new ThreadStart(DoWork));
+                _workers[i].Start();
+
+            }
+        }
+
+        public void Stop()
+        {
+            for (var i = 0; i < _workerThreads; i++)
+            {
+                _workers[i].Abort();
+            }
+        }
+
+
+        private void DoWork()
+        {
+            while (true)
+            {
+                KeyValuePair<string, byte[]> val;
+
+                lock (_lock)
+                {
+                    val = _work.FirstOrDefault();
+                    if (val.Key != null)
+                    {
+                        _work.Remove(val.Key);
+                    }
+                }
+
+                if (val.Key == null)
+                    {
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+
+                
+
+                var len = val.Value.Length;
+                File.WriteAllBytes(val.Key, val.Value);
+                 
+
+
+                lock (_lock)
+                {
+                    this._currentSize -= len;
+                }
+
+                //Thread.Sleep(100);
+            }
+           
+        }
+
+
+        public async Task WaitForRoom(long allocate, TimeSpan timeout )
+        {
+            long waited = 0;
+
+            if (allocate > this._maxSize)
+            {
+                throw new InvalidOperationException();
+            }
+
+            while ((_maxSize - CurrentSize) < allocate)
+            {
+                if (waited > timeout.TotalMilliseconds)
+                {
+                    throw new TimeoutException();
+                }
+                await Task.Delay(TimeSpan.FromMilliseconds(10));
+                waited += 10;
+            }
+        }
+
+        public async Task WaitComplete()
+        {
+            while (HasWork)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(100));
+            }
+        }
+
+    }
+
 }
